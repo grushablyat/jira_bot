650a040a75ef525dbd0e8aaabca0cba46ae5639e
Hector <tselousovgrigorij@gmail.com>
2024-07-05 14:04:56 +0300

    for/else instead of response_is_valid in lists handling

--- project/main.py
+++ project/main.py
@@ -58,16 +58,15 @@ def text_handler(message):
                 else:
                     next_state = UserState.LIST
                     menu_existing(BOT, chat.id)
-                    # response_is_valid = False
+
                     # # is request to jira appropriate here?
                     # for issue in jira_imitation.get_issues():
                     #     if message.text == issue.title:
                     #         next_state = UserState.ISSUE
                     #         current_issue_repo.create(user.id, issue.id)
                     #         menu_issue(BOT, chat.id, issue)
-                    #         response_is_valid = True
                     #         break
-                    # if not response_is_valid:
+                    # else:
                     #     next_state = UserState.LIST
                     #     menu_existing(BOT, chat.id)
 
@@ -104,7 +103,6 @@ def text_handler(message):
                     menu_menu(BOT, chat.id)
                     new_issue_repo.delete(user.id)
                 else:
-                    response_is_valid = False
                     # is request to jira appropriate here?
                     for project in jira_imitation.get_projects():
                         if message.text == project.title:
@@ -112,9 +110,8 @@ def text_handler(message):
                             new_issue_repo.create(user.id)
                             new_issue_repo.update_project(user.id, project.title)
                             menu_new_issue_title(BOT, chat.id)
-                            response_is_valid = True
                             break
-                    if not response_is_valid:
+                    else:
                         next_state = UserState.NEW_ISSUE_PROJECT
                         menu_existing(BOT, chat.id)
 
@@ -134,16 +131,14 @@ def text_handler(message):
                     menu_menu(BOT, chat.id)
                     new_issue_repo.delete(user.id)
                 else:
-                    response_is_valid = False
                     # is request to jira appropriate here?
                     for assignee in jira_imitation.get_assignees():
                         if message.text == assignee.name:
                             next_state = UserState.NEW_ISSUE_DESCRIPTION
                             new_issue_repo.update_assignee(user.id, assignee.name)
                             menu_new_issue_description(BOT, chat.id)
-                            response_is_valid = True
                             break
-                    if not response_is_valid:
+                    else:
                         next_state = UserState.NEW_ISSUE_ASSIGNEE
                         menu_existing(BOT, chat.id)
 
@@ -202,16 +197,15 @@ def callback_inline(call):
                 # BOT.edit_message_text(chat_id=chat.id, message_id=call.message.message_id,
                 #                       text='Список задач', reply_markup=None)
                 BOT.delete_message(chat.id, call.message.message_id)
-                response_is_valid = False
+
                 # is request to jira appropriate here?
                 for issue in jira_imitation.get_issues():
                     if call.data == issue.title:
                         next_state = UserState.ISSUE
                         current_issue_repo.create(user.id, issue.id)
                         menu_issue(BOT, chat.id, issue)
-                        response_is_valid = True
                         break
-                if not response_is_valid:
+                else:
                     next_state = UserState.LIST
                     menu_existing(BOT, chat.id)
 
