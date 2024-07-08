6b30af7e031e9006509d1d000da6985a94dd7e8d
Hector <tselousovgrigorij@gmail.com>
2024-07-08 20:15:29 +0300

    DB connection config
    Config is used only in create_connection() method
    In addition, comments was removed

--- project/service/repo.py
+++ project/service/repo.py
@@ -3,49 +3,44 @@ from psycopg2 import connect, OperationalError
 from project.config import DBC
 
 
-def create_connection(db_name, db_user, db_password, db_host, db_port):
+def create_connection():
     connection = None
     try:
         connection = connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
+            database=DBC['database'],
+            user=DBC['username'],
+            password=DBC['password'],
+            host=DBC['host'],
+            port=DBC['port'],
         )
-        # print("Connection to PostgreSQL DB successful")
-    except OperationalError as e:
+    except OperationalError:
         pass
-        # print(f"The error '{e}' occurred")
 
     return connection
 
 
 def select(query):
-    connection = create_connection(DBC['database'], DBC['username'], DBC['password'], DBC['host'], DBC['port'])
+    connection = create_connection()
     cursor = connection.cursor()
     result = None
     try:
         cursor.execute(query)
         result = cursor.fetchall()
-    except OperationalError as e:
+    except OperationalError:
         pass
-        # print(f"The error '{e}' occurred")
 
     connection.close()
     return result
 
 
 def execute_query(query):
-    connection = create_connection(DBC['database'], DBC['username'], DBC['password'], DBC['host'], DBC['port'])
+    connection = create_connection()
     connection.autocommit = True
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        # print("Query executed successfully")
         return True
-    except OperationalError as e:
-        # print(f"The error '{e}' occurred")
+    except OperationalError:
         return False
     finally:
         connection.close()
